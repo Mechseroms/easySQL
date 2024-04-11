41a96544e9ba66e8d8aa69cbdb9f58a3bbab8183
Jadowyne Ulve <mechserom@gmail.com>
2024-04-10 19:20:25 -0500

    did some more refactoring and leaned more into using the table class as the holder of functions.

--- easySQL.py
+++ easySQL.py
@@ -33,7 +33,9 @@ class ImproperPath(Exception):
         super().__init__(self.message)
 
 
-def Table(*, path_to_database: pathlib.Path | str):
+def Table(path_to_database: pathlib.Path | str, *, 
+          initCreate: bool = True,
+          drop_on_create=False):
     """ easySQL decorator for table classes for easy instantiation of many of the SQL_execute strings.
 
     This class will always need these variables defined within its __init__ method;
@@ -58,73 +60,93 @@ def Table(*, path_to_database: pathlib.Path | str):
                 self.path_to_database = path_to_database
                 self.data_object = namedtuple(f"{self.name}_row", list(self.columns.keys()))
                 self.columns_validation = len([column for column, type in self.columns.items() if not type.isPrimaryKey])
-                print(self.columns_validation)
-                create_table(self, drop=False)
+                print(self.name)
+                if initCreate: self.create_table(drop=drop_on_create)
 
             def __repr__(self):
                 return f"{self.__class__.__name__} ('{self.name}'@'{self.path_to_database}')"
 
-            def get_database(self):
+            def connect(self):
                 """ Returns the database specific to this table."""
                 if isinstance(path_to_database, pathlib.Path):
                     return sqlite3.connect(database=path_to_database.absolute())
                 if isinstance(path_to_database, str):
                     return sqlite3.connect(database=path_to_database)
-
-            @property
-            def create_table(self):
-                def manufacture_create_SQL_string() -> str:
-                    """ Takes the super()'s columns dictionary and bulds parts of the SQL_execute string.
-
-                    Returns:
-                        str: middle of create table SQL_execute string.
-                    """
-                    # TODO: very crude way of doing it, research a better way.
-                    middle_string = ''
-                    current_count = 0
-                    for column_name, column_type in self.columns.items():
-                        if current_count == len(self.columns.items())-1:
-                            middle_string += f"{column_name} {column_type.normalize()}"
-                        else:
-                            middle_string += f"{column_name} {column_type.normalize()}, "
-                        current_count += 1
-                    return middle_string
-                
-                return f"CREATE TABLE {self.name} ({manufacture_create_SQL_string()});"    
             
             @property
-            def drop_table(self):
+            def _create_sql(self) -> str:
+                sql_command_string = ', '.join(
+                        [f"{column_name} {column_type.normalize()}" for column_name, column_type in self.columns.items()]
+                        )
+                return f"CREATE TABLE {self.name} ({sql_command_string});" 
+
+            @property
+            def _drop_sql(self):
                 return f"DROP TABLE {self.name};"
+
+            @property
+            def _insert_sql(self):
+                column_denotation = ', '.join(
+                    [f"{column_name}" for column_name, column_type in self.columns.items() if not column_type.isAutoIncremental]
+                    )
+                values_denotaion = ', '.join(
+                    [f"?" for column_type in self.columns.values() if not column_type.isAutoIncremental]
+                    )
+                    
+                return f"({column_denotation}) VALUES ({values_denotaion})"
+
+
+            def create_table(self, drop: bool = False):
+                if drop:
+                    self.drop_table()
+                try:
+                    with self.connect() as database:
+                        cursor = database.cursor()            
+                        cursor.execute(self._create_sql)
+                except sqlite3.OperationalError as e:
+                    print(e)   
             
-            def select_row(self, column: str = None, match = None):
+            def drop_table(self):
+                try:
+                    with self.connect() as database:
+                        cursor = database.cursor()            
+                        cursor.execute(self._drop_sql)
+                except sqlite3.OperationalError:
+                    pass
+
+            def _select_sql(self, column: str = None, match = None):
                 if column:
                     return f"SELECT * FROM {self.name} WHERE {column}= '{match}'"
                 else:
                     return f"SELECT * FROM {self.name}"
-            
-            def insert_row(self, data):
+        
+            def fetchone_from_table(self, filter: tuple[str,  Any] = None, convert_data=True):
+                if filter:
+                    query = self._select_sql(column=filter[0], match=filter[1])
+                else:
+                    query = self._select_sql()
                 
-                def manufacture_insert_SQL_String():
-                    middle_string = '('
-                    gavel_string = '('
-                    current_count = 0
-                    for column_name, type in self.columns.items():
-                        if not type.isPrimaryKey:
-                            if current_count == self.columns_validation-1:
-                                middle_string += f"{column_name})"
-                                gavel_string += f"?)"
-                            else:
-                                middle_string += f"{column_name}, "
-                                gavel_string += f"?, "
-                            current_count += 1
-                    return f"{middle_string} VALUES {gavel_string}"
+                with self.connect() as database:
+                    cursor = database.cursor()
+                    cursor.execute(query)
+
+                    if not convert_data:
+                        return cursor.fetchone()
                 
+                return self.convert_data(cursor.fetchone())
+
+            def insert_row(self, data: tuple):
                 query = namedtuple('Query', ['query', 'data'])
                 if len(data) == self.columns_validation:
-                    return query(query=f"INSERT INTO {self.name}{manufacture_insert_SQL_String()}", data=data)
+                    query = query(query=f"INSERT INTO {self.name}{self._insert_sql}", data=data)
                 else:
-                    return query(query=False, data= f"passed data to {self.name} is not the right length of entries")
-            
+                    query = query(query=False, data= f"passed data to {self.name} is not the right length of entries")
+
+                assert query.query, query.data
+                with self.connect() as database:
+                    cursor = database.cursor()
+                    cursor.execute(query.query, query.data)
+
             def update_row_by_id(self, data: dict, id: str):
                 """ Update a row at {id} with {data}.
 
@@ -153,7 +175,7 @@ def Table(*, path_to_database: pathlib.Path | str):
                     return middle_string
                 return f"UPDATE {self.name} SET{manufactur_update_SQL_string(data)} WHERE id = {id}"
 
-            def convert_data(self, rows: list | tuple):
+            def convert_data(self, rows: list | tuple) -> Any:
                 """ Takes rows returned by the tables SQL_select string and returns them as namedtuples.
 
                 Args:
@@ -164,80 +186,19 @@ def Table(*, path_to_database: pathlib.Path | str):
                 """
                 self.keys = list(self.columns.keys())
                 if isinstance(rows, list):
-                    return [self.data_object(**{key: data[i+1] for i, key in enumerate(self.keys)}) for  data in rows]
+                    return [self.data_object(**{key: data[i] for i, key in enumerate(self.keys)}) for  data in rows]
                 if isinstance(rows, tuple):
-                    return [self.data_object(**{key: rows[i+1] for i, key in enumerate(self.keys)})][0]
+                    return [self.data_object(**{key: rows[i] for i, key in enumerate(self.keys)})][0]
             
             
         return Table
     return wrapper
-
-def basic_query(table, query: str):
-    """ Used as single query functions, ex. creating tables, dropping tables, updating rows
-
-    Args:
-        query (str): SQL_execute string
-    """
-    with table.get_database() as database:
-        cursor = database.cursor()            
-        cursor.execute(query)
-        
-def create_table(table: Table, drop=False):
-    
-    if drop:
-        drop_table(table=table)
-        
-    try:
-        with table.get_database() as database:
-            cursor = database.cursor()            
-            cursor.execute(table.create_table)
-    except sqlite3.OperationalError as e:
-        print(e)
-    
-def drop_table(table: Table):
-    try:
-        with table.get_database() as database:
-            cursor = database.cursor()            
-            cursor.execute(table.drop_table)
-    except sqlite3.OperationalError:
-        pass
-    
     
 def update_table_row_by_id(table: Table, row):
     query = table.update_row_by_id(data=row[1], id=row[0])
     with table.get_database() as database:
         cursor = database.cursor()            
         cursor.execute(query)
-
-
-def insert_into_table(table, data):
-    """ Passing a query and its data as a namedtuple will insert the query into the database.
-
-    Args:
-        query (namedtuple): (query.query = SQL_execute string, query.data = tuple of column's data)
-    """
-    query = table.insert_row(data)
-    print(query)
-    assert query.query, query.data
-    with table.get_database() as database:
-        cursor = database.cursor()
-        cursor.execute(query.query, query.data)
-    
-def fetchone_from_table(table: Table, filter: tuple[str,  Any] = None, convert_data=True) -> tuple:
-    if filter:
-        query = table.select_row(column=filter[0], match=filter[1])
-    else:
-        query = table.select_row()
-    
-    with table.get_database() as database:
-        cursor = database.cursor()
-        cursor.execute(query)
-
-        if not convert_data:
-            return cursor.fetchone()
-        
-        return table.convert_data(cursor.fetchone())
-        
     
 def fetchall_from_table(table, filter: tuple[str, Any] = None, convert_data=True) -> list:
     """ Fetches all rows from the database using passed query
--- examples.py
+++ examples.py
@@ -1,15 +1,24 @@
-import easySQL
+import easySQL, sys
 
-@easySQL.Table(path_to_database="test.sqlite")
-class MyTable:
-    """ A example table using easySQL and its TypeComplexes;
+@easySQL.Table(path_to_database="test.sqlite", initCreate=True, drop_on_create=True)
+class FoodsTable:
+    """ A example table using easySQL and its TypeComplex;
     """
     def __init__(self) -> None:
-        self.name = "foo"
-        self.columns = {
+        self.name: str = "Foods"
+        self.columns: dict[str, easySQL.TypeComplex] = {
             "id": easySQL.BASIC_PRIMARY_KEY,
-            "foo": easySQL.TypeComplex('string', isUnique=True),
-            "duh": easySQL.INTEGER
+            "name": easySQL.TypeComplex('string', isUnique=True),
+            "qty": easySQL.INTEGER,
+            "type": easySQL.STRING
         }
 
-my_table = MyTable()
+food_table = FoodsTable()
+foods = [("apple", 23, "fruit"),("banana", 5, "fruit"),("hamburger", 2, "meat"),("milk", 1, "dairy")]
+
+for food in foods:
+    food_table.insert_row(food)
+
+food = food_table.fetchone_from_table(("id", 4))
+
+print(food.name)
