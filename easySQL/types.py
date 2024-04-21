cbc89cd809343ab3a438608d9c197d46989ff8e4
Jadowyne Ulve <mechserom@gmail.com>
2024-04-21 10:08:10 -0500

    ...

--- easySQL/types.py
+++ easySQL/types.py
@@ -21,6 +21,7 @@ class TypeComplex:
         if data:
             return True
         else:
+            print(self, data)
             raise ValidationFailed
 
     def transform(self, data):
@@ -30,18 +31,34 @@ class TypeComplex:
         if self.validate(data): return self.transform(data)
                  
 class JSONTypeComplex(TypeComplex):
-    def transform(self, data: list | dict):
+    def transform(self, data: list | dict | str):
         return json.dumps(data)
 
-
+class ChoiceComplex(TypeComplex):
+    def validate(self, data):
+        choices = ['In', 'Out']
+        if data in choices:
+            return True
+        else:
+            raise ValidationFailed
+        
 STRING = TypeComplex(type='string')
 STRING.__doc__ = "This is a basic TypeComplex to create a column in SQL that expects a string type."
 
 INTEGER = TypeComplex(type='integer')
 INTEGER.__doc__ = "This is a basic TypeComplex to Create a column in SQL that expects a integer type."
 
+def validate(data):
+    if data or str(data) == "0":
+            return True
+    else:
+        raise ValidationFailed
+    
+INTEGER.validate = validate
+
 ID = TypeComplex(type='integer', isPrimaryKey=True, isAutoIncremental=True)
 ID.__doc__ = "This TypeComplex creates an Auto Incrementing Primary Key Integer often refered to as ID in many tables."
 
-JSON = JSONTypeComplex('string') # TODO: create a function for converting lists and dict into json string and back
+JSON = JSONTypeComplex(type='string') # TODO: create a function for converting lists and dict into json string and back
 
+CHOICE = ChoiceComplex(type="string")
